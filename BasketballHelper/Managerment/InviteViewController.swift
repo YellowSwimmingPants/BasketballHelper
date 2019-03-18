85bb5bbba0831730104ec09b73bf17ebc7c32e58
psykaping <psykaping@icloud.com>
2019-03-18 16:33:48 +0800

    3/18 Management layout ok

---
+++ BasketballHelper/Managerment/InviteViewController.swift
@@ -0,0 +1,30 @@
+//
+//  InviteViewController.swift
+//  BasketballHelper
+//
+//  Created by 王克平 on 2019/3/18.
+//  Copyright © 2019 李宜銓. All rights reserved.
+//
+
+import UIKit
+
+class InviteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
