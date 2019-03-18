ef20fa3681cc64cece3751c1dd1e601f33959c59
psykaping <psykaping@icloud.com>
2019-03-18 17:29:03 +0800

    3/18 02

--- BasketballHelper/BillBoard/BillBoardTableViewController.swift
+++ BasketballHelper/BillBoard/BillBoardTableViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class BillBoardTableViewController: UITableViewController {
-
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var contentTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
