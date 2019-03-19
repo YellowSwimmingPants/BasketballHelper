d6412e8056355e962b1bb3b799e5b4dd64040fb5
chennanyu <xpopen4000@hotmail.com.tw>
2019-03-19 17:13:00 +0800

    Add insert function Defaults

---
+++ BasketballHelper/PlayerInfo/Common.swift
@@ -0,0 +1,7 @@
+import Foundation
+import UIKit
+
+// 實機
+// let common_url = "http://192.168.0.101:8080/Spot_MySQL_Web/"
+// 模擬器
+let common_url = "http://127.0.0.1:8080/BasketBallProject/"
--- BasketballHelper/PlayerInfo/PlayerList.swift
+++ BasketballHelper/PlayerInfo/PlayerList.swift
@@ -9,11 +9,28 @@
 import UIKit
 
 class PlayerList: UITableViewController {
+    let url_server = URL(string: common_url + "PlayerList")
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
+        tableViewAddRefreshControl()
+    }
+    
+      /** tableView加上下拉更新功能 */
+    func tableViewAddRefreshControl() {
+        let refreshControl = UIRefreshControl()
+        refreshControl.attributedTitle = NSAttributedString(string: "Pull to refresh")//attributedTitle標題屬性
+        refreshControl.addTarget(self, action: #selector(showAllSpots), for: .valueChanged)
+        //addTarget 指下拉動作做觸發
+        self.tableView.refreshControl = refreshControl
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        showAllSpots()
+    }
+    
+    @objc func showAllSpots(){
+        
     }
 
 
