406309fddba577b026f6ecf373ea1ae8a4d0feb6
psykaping <psykaping@icloud.com>
2019-03-21 18:26:28 +0800

    3/21 05

---
+++ BasketballHelper/Register/HomepageViewController.swift
@@ -0,0 +1,26 @@
+//
+//  HomepageViewController.swift
+//  BasketballHelper
+//
+//  Created by 王克平 on 2019/3/21.
+//  Copyright © 2019 李宜銓. All rights reserved.
+//
+
+import UIKit
+
+class HomepageViewController: UIViewController {
+    
+    @IBOutlet weak var accountTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    @IBAction func clickSignIn(_ sender: Any) {
+    }
+    
+
+}
