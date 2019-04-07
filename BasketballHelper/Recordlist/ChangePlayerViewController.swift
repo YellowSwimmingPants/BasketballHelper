3dd21d12e07d8e28caaa46adb608f3936df4d247
witch26demo <witch26demo@gmail.com>
2019-04-07 19:24:05 +0800

    4/7 fix v2

--- BasketballHelper/Recordlist/ChangePlayerViewController.swift
+++ BasketballHelper/Recordlist/ChangePlayerViewController.swift
@@ -2,6 +2,9 @@ import UIKit
 
 class ChangePlayerViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
+//    var players = [Player]()
+//    let url_server = URL(string: common_url + "PlayerServlet")
+    
     var players = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17"]
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -25,5 +28,37 @@ class ChangePlayerViewController: UIViewController, UITableViewDataSource, UITab
         super.viewDidLoad()
 
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        showAllPlayers()
+    }
 
+    @objc func showAllPlayers() {
+//        let requestParam = ["action" : "getAll"]
+//        executeTask(url_server!, requestParam) { (data, response, error) in
+//            if error == nil {
+//                if data != nil {
+//                    // 將輸入資料列印出來除錯用
+//                    print("input: \(String(data: data!, encoding: .utf8)!)")
+//
+//                    if let result = try? JSONDecoder().decode([Spot].self, from: data!) {
+//                        self.spots = result
+//                        DispatchQueue.main.async {
+//                            if let control = self.tableView.refreshControl {
+//                                if control.isRefreshing {
+//                                    // 停止下拉更新動作
+//                                    control.endRefreshing()
+//                                }
+//                            }
+//                            /* 抓到資料後重刷table view */
+//                            self.tableView.reloadData()
+//                        }
+//                    }
+//                }
+//            } else {
+//                print(error!.localizedDescription)
+//            }
+//        }
+    }
+    
 }
---
+++ BasketballHelper/Recordlist/PeriodTableViewController.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+class PeriodTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    @IBAction func clickCancel(_ sender: Any) {
+         let gameRecordTVC = self.storyboard?.instantiateViewController(withIdentifier: "gameRecordTVC") as! GameRecordTableViewController
+        self.navigationController?.pushViewController(gameRecordTVC, animated: true)
+    }
+    
+    @IBAction func clickDone(_ sender: Any) {
+        
+    }
+    
+    
+
+}
