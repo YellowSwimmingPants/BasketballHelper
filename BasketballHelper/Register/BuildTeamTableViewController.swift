e9bab42bb2b8bfe2aedbb4b0a04ee37993514c58
psykaping <psykaping@gmail.com>
2019-04-12 17:45:27 +0800

    4/12 04 fix

--- BasketballHelper/Register/BuildTeamTableViewController.swift
+++ BasketballHelper/Register/BuildTeamTableViewController.swift
@@ -30,6 +30,66 @@ class BuildTeamTableViewController: UITableViewController {
             showToast(view: self.view, message: "請輸入隊名")
             return
         }
+        var teamInfo = [String: String]()
+        teamInfo["action"] = "teamExist"
+        teamInfo["teamInfo"] = name
+        executeTask(url_server!, teamInfo) { (data, response, error) in
+            if error == nil {
+                if data != nil {
+                    if let result = String(data: data!, encoding: .utf8) {
+                        if let count = Int(result) {
+                            DispatchQueue.main.async {
+                                if count != 0 {
+                                    let alert = UIAlertController(title: "\(name!)隊已存在", message: "是否要加入此球隊？", preferredStyle: .alert)
+                                    alert.addAction(UIAlertAction(title: "YES", style: .destructive, handler: { (action) in
+                                        self.joinTeam(name!)
+                                    }))
+                                    alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+                                    self.present(alert, animated: true)
+                                } else {
+                                    self.createTeam(name!)
+                                }
+                            }
+                        }
+                    }
+                }
+            } else {
+                print(error!.localizedDescription)
+            }
+        }
+    }
+    
+    func joinTeam(_ name: String) {
+        var teamInfo = [String : String]()
+        teamInfo["action"] = "joinTeam"
+        teamInfo["teamInfo"] = name
+        teamInfo["userAccount"] = users.userAccount
+        teamInfo["userPassword"] = users.userPassword
+        executeTask(url_server!, teamInfo) { (data, response, error) in
+            if error == nil {
+                if data != nil {
+                    if let result = try? JSONDecoder().decode([String : String].self, from: data!) {
+                        DispatchQueue.main.async {
+                            if result["success"] == "Yes" {
+                                let userInfo = result["userInfo"]
+                                let login = try? JSONDecoder().decode(UserInfo.self, from: userInfo!.data(using: .utf8)!)
+                                let loginOK = try! JSONEncoder().encode(login)
+                                self.userDefault.set(loginOK, forKey: "userDefault")
+                                self.viewController = self.storyboard!.instantiateViewController(withIdentifier: "Homepage")
+                                self.present(self.viewController, animated: true, completion: nil)
+                            } else {
+                                showToast(view: self.view, message: "建立失敗")
+                            }
+                        }
+                    }
+                }
+            } else {
+                print(error!.localizedDescription)
+            }
+        }
+    }
+    
+    func createTeam(_ name: String) {
         var teamInfo = [String : String]()
         teamInfo["action"] = "createTeam"
         teamInfo["teamInfo"] = name
