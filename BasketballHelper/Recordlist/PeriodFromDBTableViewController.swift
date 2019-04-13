935006a66a04614440f98f760e0aeedb9ce67326
witch26demo <witch26demo@gmail.com>
2019-04-13 20:20:06 +0800

    4/13 fix v4

--- BasketballHelper/Recordlist/PeriodFromDBTableViewController.swift
+++ BasketballHelper/Recordlist/PeriodFromDBTableViewController.swift
@@ -2,10 +2,13 @@ import UIKit
 
 class PeriodFromDBTableViewController: UITableViewController {
     let url_server = URL(string: common_url_user + "GameServlet")
+    var gameDatas = [GameDataCount]()
+    var gameName: String?
+    var period: Int?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.title = gameName
     }
 
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -17,5 +20,24 @@ class PeriodFromDBTableViewController: UITableViewController {
 
         return 5
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let controller = segue.destination as? PlayersEachPeriodTableViewController {
+            if segue.identifier == "PeriodOne" {
+                period = 1
+            } else if segue.identifier == "PeriodTwo" {
+                period = 2
+            } else if segue.identifier == "PeriodThree" {
+                period = 3
+            } else if segue.identifier == "PeriodFour" {
+                period = 4
+            } else if segue.identifier == "OverTime" {
+                period = 5
+            }
+            
+            controller.period = period
+        }
+    }
+    
 
 }
--- BasketballHelper/Recordlist/PlayerDataCountViewController.swift
+++ BasketballHelper/Recordlist/PlayerDataCountViewController.swift
@@ -1,30 +1,78 @@
-//
-//  PlayerDataCountViewController.swift
-//  BasketballHelper
-//
-//  Created by 李宜銓 on 2019/4/13.
-//  Copyright © 2019 李宜銓. All rights reserved.
-//llllllllllllll
-
 import UIKit
 
 class PlayerDataCountViewController: UIViewController {
-
+    @IBOutlet weak var totalScore: UILabel!
+    @IBOutlet weak var ft: UILabel!
+    @IBOutlet weak var ftl: UILabel!
+    @IBOutlet weak var ftPercentage: UILabel!
+    @IBOutlet weak var fg: UILabel!
+    @IBOutlet weak var fgl: UILabel!
+    @IBOutlet weak var fgPecentage: UILabel!
+    @IBOutlet weak var tpm: UILabel!
+    @IBOutlet weak var tpl: UILabel!
+    @IBOutlet weak var tpPercentage: UILabel!
+    @IBOutlet weak var assist: UILabel!
+    @IBOutlet weak var steal: UILabel!
+    @IBOutlet weak var block: UILabel!
+    @IBOutlet weak var ofnReb: UILabel!
+    @IBOutlet weak var defReb: UILabel!
+    @IBOutlet weak var foul: UILabel!
+    @IBOutlet weak var turnOver: UILabel!
+    
+    let url_server = URL(string: common_url_playerInfo + "PlayerServlet")
+    var player : Page_playerList!
+    var playerData : GameDataCount!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        showData()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func showData(){
+        var requestParam = [String : Any]()
+        requestParam["action"] = "getData"
+        requestParam["playerID"] = player.playerID
+        print(player.playerID)
+        executeTask(url_server!, requestParam) { (data, response, error) in
+            if error == nil {
+                if data != nil {
+                    // 將輸入資料列印出來除錯用
+                    print("input: \(String(data: data!, encoding: .utf8)!)")
+                    DispatchQueue.main.async {
+                        self.dataShow(data!)
+                    }
+                }
+            } else {
+                print(error!.localizedDescription)
+            }
+        }
     }
-    */
-
+    
+    func dataShow(_ data: Data) {
+        playerData = try? JSONDecoder().decode(GameDataCount.self, from: data)
+        //        print(String(describing: playdata!.Assist!))
+        ft.text = String(describing: playerData!.FT!)
+        ftl.text = String(describing: playerData!.FTL!)
+        fg.text = String(describing: playerData!.FG!)
+        fgl.text = String(describing:playerData!.FGL!)
+        tpm.text = String(describing:playerData!.TPM!)
+        tpl.text = String(describing:playerData!.TPL!)
+        foul.text = String(describing:playerData!.Foul!)
+        ofnReb.text = String(describing:playerData!.OfnReb!)
+        defReb.text = String(describing: playerData!.DefReb!)
+        turnOver.text = String(describing: playerData!.Assist!)
+        steal.text = String(describing: playerData!.Steal!)
+        block.text = String(describing: playerData!.Block!)
+        assist.text = String(describing: playerData!.Assist!)
+        
+        ftPercentage.text = String(format: "%.1f",((Double(playerData!.FT!) / (Double(playerData!.FT!+playerData!.FTL!))) * 100))
+        
+        fgPecentage.text = String(format: "%.1f",((Double(playerData!.FG!) / (Double(playerData!.FG!+playerData!.FGL!))) * 100))
+        
+        tpPercentage.text = String(format: "%.1f",((Double(playerData!.TPM!) / (Double(playerData!.TPM!+playerData!.TPL!))) * 100))
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
 }
